71bcdfbb1488537aac017eeff71064bd5ca15bf1
chuuuuu <33323263+chuuuuu@users.noreply.github.com>
2021-07-09 08:46:50 +0800

    fix: using function component

--- react_ts_eslint_example/src/App.tsx
+++ react_ts_eslint_example/src/App.tsx
@@ -1,26 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-function App(): React.ReactElement {
+type AppProp = {
+  foo: string,
+  bar: string,
+};
+
+const App = ({foo, bar}: AppProp): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div> hello {foo} </div>
+      <div> hello {bar} </div>
     </div>
   );
-}
+};
 
 export default App;
