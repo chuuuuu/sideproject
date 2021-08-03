d76fee6d35caca0778ea8872c1720a439c9b9526
chuuuuu <33323263+chuuuuu@users.noreply.github.com>
2021-08-03 21:12:29 +0800

    use graphql-ws and apolloserver 3.0

--- chatroom_with_graphql/client/src/pages/_app.tsx
+++ chatroom_with_graphql/client/src/pages/_app.tsx
@@ -1,36 +1,44 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { Provider, Client, defaultExchanges, subscriptionExchange } from "urql";
-import { SubscriptionClient } from "subscriptions-transport-ws";
+import {
+  Provider,
+  createClient,
+  defaultExchanges,
+  subscriptionExchange,
+} from "urql";
 import theme from "../theme";
+import { createClient as createWSClient } from "graphql-ws";
 
 const getClient = () => {
-  if (process.browser) {
-    const subscriptionClient = new SubscriptionClient(
-      "ws://localhost:8000/graphql",
-      {
-        reconnect: true,
-      }
-    );
-
-    return new Client({
+  if (!process.browser) {
+    return createClient({
       url: "http://localhost:8000/graphql",
-      exchanges: [
-        ...defaultExchanges,
-        subscriptionExchange({
-          forwardSubscription(operation) {
-            return subscriptionClient?.request(operation);
-          },
-        }),
-      ],
+      exchanges: [...defaultExchanges],
       fetchOptions: {
         credentials: "include",
       },
     });
   }
 
-  return new Client({
+  const wsClient = createWSClient({
+    url: "ws://localhost:8000/graphql",
+  });
+  return createClient({
     url: "http://localhost:8000/graphql",
-    exchanges: [...defaultExchanges],
+    exchanges: [
+      ...defaultExchanges,
+      subscriptionExchange({
+        forwardSubscription(operation) {
+          return {
+            subscribe: (sink) => {
+              const dispose = wsClient.subscribe(operation, sink);
+              return {
+                unsubscribe: dispose,
+              };
+            },
+          };
+        },
+      }),
+    ],
     fetchOptions: {
       credentials: "include",
     },
--- chatroom_with_graphql/server/src/index.ts
+++ chatroom_with_graphql/server/src/index.ts
@@ -4,10 +4,9 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { MessageResolver } from "./resolvers/message";
-import { createServer } from "http";
-import { SubscriptionServer } from "subscriptions-transport-ws";
-import { execute, subscribe } from "graphql";
 import cors from "cors";
+import ws from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 
 const main = async () => {
   const app = express();
@@ -18,39 +17,29 @@ const main = async () => {
     })
   );
 
-  const httpServer = createServer(app);
   const schema = await buildSchema({
     resolvers: [HelloResolver, MessageResolver],
     validate: false,
   });
+
   const apolloServer = new ApolloServer({
     schema: schema,
   });
+  await apolloServer.start();
 
   apolloServer.applyMiddleware({
     app,
     cors: false,
   });
 
-  apolloServer.installSubscriptionHandlers
-
-  SubscriptionServer.create(
-    {
-      // This is the `schema` we just created.
-      schema,
-      // These are imported from `graphql`.
-      execute,
-      subscribe,
-    },
-    {
-      // This is the `httpServer` we created in a previous step.
-      server: httpServer,
-      // This `server` is the instance returned from `new ApolloServer`.
-      path: apolloServer.graphqlPath,
-    }
-  );
-
-  httpServer.listen(8000, () => {
+  const server = app.listen(8000, () => {
+    // create and use the websocket server
+    const wsServer = new ws.Server({
+      server,
+      path: '/graphql',
+    });
+  
+    useServer({ schema }, wsServer);
     console.log("server started on localhost:8000");
   });
 };
